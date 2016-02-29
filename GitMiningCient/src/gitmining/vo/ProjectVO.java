732d27b4f96c5a5380aea5d977f99950b76acbd1
qwertyuikj <1543185323@qq.com>
2016-02-29 19:49:39 +0800

    初步完善vo类

--- GitMiningCient/src/gitmining/vo/ProjectVO.java
+++ GitMiningCient/src/gitmining/vo/ProjectVO.java
@@ -1,5 +1,100 @@
 package gitmining.vo;
 
 public class ProjectVO {
+	private String name;
+	private String language;
+	private String HTTPS;
+	private UserVO author;
+	private int commits;
+	private int branches;
+	private int releases;
+	private int contributors;
+	
+	private int issues;
+	private int pullrequests;
+	
+	public ProjectVO(String name, String language, String HTTPS, UserVO author,
+			int commits, int branches, int releases, 
+			int contributors, int issues, int pullrequests){
+		this.name = name;
+		this.language = language;
+		this.HTTPS = HTTPS;
+		this.author = author;
+		this.commits = commits;
+		this.branches = branches;
+		this.releases = releases;
+		this.contributors = contributors;
+		this.issues = issues;
+		this.pullrequests = pullrequests;
+	}
 
+  	public String getName() {
+  		return name;
+  	}
+  	public void setName(String name) {
+  		this.name = name;
+  	}
+  	
+  	public String getLanguage() {
+  		return language;
+  	}
+  	public void setLanguage(String language) {
+  		this.language = language;
+  	}
+
+  	public String getHTTPS() {
+  		return HTTPS;
+  	}
+  	public void setHTTPS(String HTTPS) {
+  		this.HTTPS = HTTPS;
+  	}
+
+  	public UserVO getAuthor() {
+  		return author;
+  	}
+  	public void setAuthor(UserVO author) {
+  		this.author = author;
+  	}
+
+  	public int getcommits() {
+  		return commits;
+  	}
+  	public void setcommits(int commits) {
+  		this.commits = commits;
+  	}
+
+  	public int getbranches() {
+  		return branches;
+  	}
+  	public void setbranches(int branches) {
+  		this.branches = branches;
+  	}
+
+  	public int getreleases() {
+  		return releases;
+  	}
+  	public void setreleases(int releases) {
+  		this.releases = releases;
+  	}
+
+  	public int getcontributors() {
+  		return contributors;
+  	}
+  	public void setcontributors(int contributors) {
+  		this.contributors = contributors;
+  	}
+
+  	public int getissues() {
+  		return issues;
+  	}
+  	public void setissues(int issues) {
+  		this.issues = issues;
+  	}
+
+  	public int getpullrequests() {
+  		return pullrequests;
+  	}
+  	public void setpullrequests(int pullrequests) {
+  		this.pullrequests = pullrequests;
+  	}
 }
--- GitMiningCient/src/gitmining/vo/UserVO.java
+++ GitMiningCient/src/gitmining/vo/UserVO.java
@@ -1,5 +1,94 @@
 package gitmining.vo;
 
-public class UserVO {
+import java.util.ArrayList;
 
+public class UserVO {
+	private String name;
+	private String address;
+	private String email;
+	private String jointime;
+	private ArrayList<UserVO> Followers;//粉丝
+	private ArrayList<UserVO> Starred;
+	private ArrayList<UserVO> Following;//偶像
+
+	private ArrayList<ProjectVO> contributions;
+	private ArrayList<ProjectVO> repositories;
+	
+	
+     public UserVO(String name, String address, String email, String jointime){
+    	 this.name = name;
+    	 this.address = address;
+    	 this.email = email;
+    	 this.jointime = jointime;
+    	 
+    	 Followers = new ArrayList<UserVO>();
+    	 Starred = new ArrayList<UserVO>();
+    	 Following = new ArrayList<UserVO>();
+    	 contributions = new ArrayList<ProjectVO>();
+    	 repositories = new ArrayList<ProjectVO>();
+     }
+
+     
+  	public String getName() {
+  		return name;
+  	}
+  	public void setName(String name) {
+  		this.name = name;
+  	}
+    
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+    
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+    
+	public String getJointime() {
+		return jointime;
+	}
+	public void setJointime(String jointime) {
+		this.jointime = jointime;
+	}
+    
+	public ArrayList<UserVO> getFollowers() {
+		return Followers;
+	}
+	public void setFollowers(ArrayList<UserVO> Followers) {
+		this.Followers = Followers;
+	}
+    
+	public ArrayList<UserVO> getStarred() {
+		return Starred;
+	}
+	public void setStarred(ArrayList<UserVO> Starred) {
+		this.Starred = Starred;
+	}
+    
+	public ArrayList<UserVO> getFollowing() {
+		return Following;
+	}
+	public void setFollowing(ArrayList<UserVO> Following) {
+		this.Following = Following;
+	}
+    
+	public ArrayList<ProjectVO> getContributions() {
+		return contributions;
+	}
+	public void setContributions(ArrayList<ProjectVO> contributions) {
+		this.contributions = contributions;
+	}
+    
+	public ArrayList<ProjectVO> getRepositories() {
+		return repositories;
+	}
+	public void setRepositories(ArrayList<ProjectVO> repositories) {
+		this.repositories = repositories;
+	}
 }
